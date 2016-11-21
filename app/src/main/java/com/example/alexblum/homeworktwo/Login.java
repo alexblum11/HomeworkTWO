19e2275a7054acf4c3aa40f2e394f3cdcd9bcb2a
pinjam100 <alexblum@umich.edu>
2016-11-20 19:39:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alexblum/homeworktwo/Login.java
@@ -0,0 +1,63 @@
+package com.example.alexblum.homeworktwo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class Login extends Activity implements View.OnClickListener{
+
+    //declare widgets
+    private Button button_login;
+    private EditText edit_username;
+    private EditText edit_password;
+    private RadioButton rb_profile;
+    private RadioButton rb_settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Link Widgets to UI
+        button_login = (Button) findViewById(R.id.button_login);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        edit_username = (EditText) findViewById(R.id.edit_username);
+        rb_profile = (RadioButton) findViewById(R.id.rb_profile);
+        rb_settings = (RadioButton) findViewById(R.id.rb_settings);
+
+        button_login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(edit_username.getText().toString().equals("user") & edit_password.getText().toString().equals("1234")) {
+            // Correct login credentials
+            if(rb_profile.isChecked()) {
+                Intent intent = new Intent(this, Profile.class);
+                startActivity(intent);
+            } else {
+                if (rb_settings.isChecked()) {
+                    Intent intent = new Intent(this, Settings.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Please choose a Screen!", Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            // Wrong login credentials
+            Toast.makeText(this, "Login denied. \n\nMarvin is sad.", Toast.LENGTH_LONG).show();
+            edit_username.setText("");
+            edit_password.setText("");
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/alexblum/homeworktwo/Settings.java
@@ -0,0 +1,27 @@
+package com.example.alexblum.homeworktwo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Settings extends Activity implements View.OnClickListener{
+
+    private Button button_logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        button_logout = (Button) findViewById(R.id.button_logout);
+        button_logout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+}
